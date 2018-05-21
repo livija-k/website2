bb4655376cbb11496d7f30d017542ead59b8a9f4
livija-k <39097492+livija-k@users.noreply.github.com>
2018-05-21 20:44:47 +0300

    Restore login.php

--- login.php
+++ login.php
@@ -1,13 +1,81 @@
- <?php 
-       include_once 'db.php';
-       include_once 'utils.php';
-  ?>
+<?php 
+include_once 'db.php';
+include_once 'utils.php';
+
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+  // Check if username is empty
+  if(empty(trim($_POST["username"]))){
+    $username_err = 'Please enter username.';
+  } else{
+    $username = trim($_POST["username"]);
+  }
+
+  // Check if password is empty
+  if(empty($_POST['password'])){
+    $password_err = 'Please enter your password.';
+  } else{
+    $password = $_POST['password'];
+  }
+
+  // Validate credentials
+  if(empty($username_err) && empty($password_err)){
+    // Prepare a select statement
+    $sql = "SELECT username, password FROM users WHERE username = ?";
+
+    if($stmt = $conn->prepare($sql)){
+      // Bind variables to the prepared statement as parameters
+      $stmt->bind_param("s", $param_username);
+
+      // Set parameters
+      $param_username = $username;
+
+      // Attempt to execute the prepared statement
+      if($stmt->execute()){
+        // Store result
+        $stmt->store_result();
+
+        // Check if username exists, if yes then verify password
+        if($stmt->num_rows == 1){                    
+          // Bind result variables
+          $stmt->bind_result($username, $hashed_password);
+          if($stmt->fetch()){
+            if(password_verify($password, $hashed_password)){
+              /* Password is correct, so start a new session and
+                            save the username to the session */
+              session_start();
+              $_SESSION['username'] = $username;      
+              header("location: index.php");
+            } else{
+              // Display an error message if password is not valid
+              $password_err = 'The password you entered was not valid.';
+            }
+          }
+        } else{
+          // Display an error message if username doesn't exist
+          $username_err = 'No account found with that username.';
+        }
+      } else{
+        $username_err = "Oops! Something went wrong. Please try again later.";
+      }
+    }
+
+    // Close statement
+    $stmt->close();
+  }
+}
+?>
 <html lang="en"><head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Signin Template for Bootstrap</title>
+    <title>Signin</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
@@ -18,12 +86,25 @@
 
   <body class="text-center">
     <form class="form-signin" action="<?php echo getValueOr($_SERVER["PHP_SELF"]); ?>" method="post">
-<!--       <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"> -->
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <?php
+        if (!empty($username_err))
+        {
+          echo '<div class="alert alert-danger" role="alert">
+                  '.$username_err.'
+                </div>';
+        }
+        if (!empty($password_err))
+        {
+          echo '<div class="alert alert-danger" role="alert">
+                  '.$password_err.'
+                </div>';
+        }
+      ?>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input name="username" type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" value="<?php echo $username; ?>">
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+      <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
       <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
@@ -32,6 +113,5 @@
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
     </form>
-  
-
-</body></html>
+  </body>
+</html>
